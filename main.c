e54f3713534df8b6558f84e9d11e5a6a85be86c7
Jeremy Laughlin <jeremylaughlin@Jeremys-MacBook-Pro-2.local>
2019-10-27 12:51:54 -0700

    Boring changes and spelling correction and formatting improvements

--- main.c
+++ main.c
@@ -10,54 +10,64 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-void stringSort(int*strings, int nstring, int (*compfun)());
+
+void stringSort(int* strings, int nstring, int (*compfun)());
 int numsstrcmp(int string1, int string2);
-void printArray(int*array, int size);
+void printArrayPointerValue(int** array, int size);
+void printArray(int* array, int size);
 /***********************************************
  * Description: Read int the file and send it to the files to be sorted
  * Input:the name of the file
  * Output: no true output
 ***********************************************/
 int main(int argc, char *argv[]) {
-    if(argc < 2){
+    
+    if(argc < 2)
+    {
         printf("Please input the file name\n");
         return 0;
     }
     FILE *fp;
     char FirstLine[36];
+    
     fp = fopen(argv[1], "r");
-    if (fp == 0) {
+    if (fp == 0)
+    {
         printf("Error opening the file\n");
         exit(0);
     }
     int lines = 0;//line marker
     fscanf(fp,"%s",FirstLine) ;
-    int nubLine = FirstLine[0]-'0';
-    printf("%d list\n\nPrints the sorted lines\n", nubLine);
+    int numLine = FirstLine[0]-'0';
+    printf("%d list\n\nPrints the sorted lines prior to sort\n", numLine);
 
-    int **line=malloc(sizeof(int)* nubLine);
-    int*fine = malloc(sizeof(int)* nubLine);
-    int*origingal = malloc(sizeof(int)* nubLine);
-    int*shift = malloc(sizeof(int)* nubLine);
-    while (lines <nubLine){
+    int **line=malloc(sizeof(int)* numLine);
+    int *final = malloc(sizeof(int)* numLine);
+    int *original = malloc(sizeof(int)* numLine);
+    int *shift = malloc(sizeof(int)* numLine);
+    while (lines < numLine)
+    {
 
         char lineRead[100];
         fscanf(fp,"%s", lineRead);
         int numbers = lineRead[0]-'0';
-        *(line+lines)= malloc( sizeof(int)*nubLine);
+        *(line+lines)= malloc( sizeof(int)*numbers);
         int  set[numbers];
-        *(fine+lines) = numbers;
-        *(origingal+lines) = numbers;
+        *(final+lines) = numbers;
+        *(original+lines) = numbers;
         int setnumb =0;
         for (int i = 1; i<strlen(lineRead);i++)
         {
-            if(lineRead[i] !=','&&lineRead[i]!=':'&&lineRead[i] !=' '){
+            if(lineRead[i] != ',' && lineRead[i] != ':' && lineRead[i] != ' ')
+            {
                 if(lineRead[i] == '-')
                 {
                     char temp = (lineRead[i+1]-'0');
                     set[setnumb] = 0-(int)temp;
                     i++;
-                } else {
+                }
+                else
+                {
                     set[setnumb] = lineRead[i]-'0';
                 }
                 setnumb++;
@@ -71,7 +81,7 @@ int main(int argc, char *argv[]) {
             key[i] = set[i];
         }
 
-        *(line+lines)= key[0];
+        **(line+lines) = key[0];
 
         printArray(key, numbers);//print the sorted line
 
@@ -79,28 +89,30 @@ int main(int argc, char *argv[]) {
 
     }
 
-    stringSort(fine, nubLine,numsstrcmp);//forting the line length
+    stringSort(final, numLine,numsstrcmp);//forting the line length
     printf("This shows the length before and after the sort\n");
-    printArray(fine, nubLine);//printing the sorted
-    printArray(origingal, nubLine);//printing the origingal order
+    printArray(original, numLine);//printing the original order
+    printArray(final, numLine);//printing the sorted
+    
     int k=0;//marker
-    for(int j =0; j<nubLine;j++)  {
+    for(int j =0; j<numLine;j++)
+    {
         k = 0;
-        while(*(fine+k)!=*(origingal+j)){
+        while(*(final+j)!=*(original+k))
+        {
             k++;
         }
         *(shift+j) = k;//shift marker
     }
-    printf("This prints the list from shortes to longest\n");
-    for(int i = 0; i<nubLine;i++){
-
-
-        printArray((line+i+ *(shift+i)), *(fine+i));//Print the final array
+    printf("This prints the list from shortest to longest\n");
+    for(int i = 0; i<numLine;i++)
+    {
+        printArrayPointerValue((line + *(shift + i)), *(final+i));//Print the final array
     }
     //freeing the memory
-free(line);
-    free(fine);
-    free(origingal);
+    free(line);
+    free(final);
+    free(original);
     return 0;
 }
 /***********************************************
@@ -108,18 +120,22 @@ free(line);
  * Input: the pointer the size and the pointer to the function
  * Output: no true output
 ***********************************************/
-void stringSort(int*strings, int nstring, int (*compfun)()){
+void stringSort(int *strings, int nstring, int (*compfun)())
+{
     int i, j;
     int didswap;
-    do{
+    do
+    {
         didswap =0;
-        for(i=0; i<nstring-1;i++){
+        for(i=0; i<nstring-1;i++)
+        {
             j=i+1;
-            if((*compfun)(*(strings+i),*(strings+j))>0){
-                char*temp = strings[i];
+            if((*compfun)(*(strings+i),*(strings+j))>0)
+            {
+                int temp = strings[i];
                 *(strings+i) = *(strings+j);
                 *(strings+j) = temp;
-                didswap =1;
+                didswap = 1;
             }
         }
     }while(didswap);
@@ -130,25 +146,48 @@ void stringSort(int*strings, int nstring, int (*compfun)()){
  * Input: the two items to be swapped
  * Output: no true output
 ***********************************************/
-int numsstrcmp(int string1, int string2){
+int numsstrcmp(int string1, int string2)
+{
     int n1 =string1;
     int n2 =string2;
-    if(n1<n2){
+    if(n1<n2)
+    {
         return -1;
-    }else if(n1==n2){
+    }
+    else if(n1==n2)
+    {
         return 0;
-    }else{
+    }
+    else
+    {
         return 1;
     }
 }
+
 /***********************************************
  * Description: print the array
  * Input: the pointer to be printed and the size
  * Output: the printed list.
 ***********************************************/
-void printArray(int*array, int size){
-    for(int i =0; i<size;i++){
+void printArray(int *array, int size)
+{
+    for(int i =0; i<size;i++)
+    {
         printf("%d ", *(array+i));
+    }
+    printf("\n\n");
+}
+
+/***********************************************
+ * Description: print the array
+ * Input: the pointer to the pointer to be printed and the size
+ * Output: the printed list.
+***********************************************/
+void printArrayPointerValue(int **array, int size)
+{
+    for(int i =0; i<size;i++)
+    {
+        printf("%d ", **(array+i));
     }
     printf("\n\n");
 }
